1a3fafbb2a5c2f55ed411e649613a358b68e47e4
David Irwin <redirwin@gmail.com>
2022-12-08 12:54:24 -0700

    Testing for mobile tap.

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,15 @@ const numOfColumns = 0;
 const difficulty = 0;
 
 $(document).ready(function () {
-  $(".hamburger").click(function () {
+  // $(".hamburger").click(function () {
+  //   $(this).toggleClass("is-active");
+  //   $("#menu").slideToggle();
+  //   $(difficultyForm).slideToggle();
+  // });
+  $(".hamburger").tap(function () {
     $(this).toggleClass("is-active");
     $("#menu").slideToggle();
     $(difficultyForm).slideToggle();
-      // $(this).css('position', 'fixed');
-      // $(this).css('right', '1.25rem');
   });
 });
 
